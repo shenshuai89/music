4d5ae6e35604bcf0ed81b5499c9374383a7bdc3e
shenshuai89 <shenshuai89@qq.com>
2017-09-10 23:26:56 +0800

    完成推荐歌单，lazyload和loading

---
+++ src/components/singer/singer.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="singer">
+      歌手页面
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+    export default{
+
+    }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,23 @@
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import fastclick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
+
+import 'common/stylus/index.styl'
+
+// 移动端300ms延迟
+fastclick.attach(document.body)
+
+Vue.config.productionTip = false
+Vue.use(VueLazyLoad, {
+  loading: require('common/image/default.png')
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
